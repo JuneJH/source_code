a6b81a6c7d381a0c1dc2d73045248a7b58238360
JuneJH <15310487388@163.com>
2022-01-27 16:46:22 +0800

    feat(*): 搭建环境

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Page from './components/测试fiber';
+;
+
 
 function App() {
+  console.log(<Page/>)
   return (
     <div className="App">
       hello react!!!
+      <Page/>
     </div>
   );
 }
